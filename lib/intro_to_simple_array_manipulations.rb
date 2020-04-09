61c42a2cc69fad7e42ac4d5451e801ed81970cb1
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-09 21:12:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
 def using_concat (my_favorite_things, more_favs)
   my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   more_favs = ["sports cars", "flatiron school"]
-  my_favorite_things.concat(my_favorite_things, more_favs)
+  puts 
 end
