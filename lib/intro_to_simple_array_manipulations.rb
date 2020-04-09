ed1b06b64c462bd0359752083efcd29c4ff18de7
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-09 21:34:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,6 @@
 def using_concat (my_favorite_things, more_favs)
-  # my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
- # more_favs = ["sports cars", "flatiron school"]
+  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+  more_favs = ["sports cars", "flatiron school"]
   puts my_favorite_things.concat("sports cars", "flatiron school")
 end
 
