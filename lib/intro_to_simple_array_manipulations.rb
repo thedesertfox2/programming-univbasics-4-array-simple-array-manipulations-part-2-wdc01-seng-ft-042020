1ccb0c7952254f5b88095a1cb5883dd795b53aa3
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-09 21:25:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,6 +4,22 @@ def using_concat (my_favorite_things, more_favs)
   my_favorite_things.concat(more_favs)
 end
 
-def
+def using_insert
 
+end
+
+def using_uniq
+  
+end
+
+def using_flatten
+  
+end
+
+def using_delete
+  
+end
+
+def using_delete_at
+  
 end
